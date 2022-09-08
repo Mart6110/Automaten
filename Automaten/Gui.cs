945d462e616b58f9e378c35911248af5583ee53c
mart6110 <martinus.mortensen@gmail.com>
2022-09-08 12:22:17 +0200

    Add project files.

---
+++ Automaten/Gui.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaten
+{
+    class Gui
+    {
+        private bool exit = false;
+
+        public void Menu()
+        {
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("-------- Vending Machine --------");
+                Console.WriteLine();
+
+
+
+            } while (exit != false);
+        }
+    }
+}
---
+++ Automaten/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Automaten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gui gui = new Gui();
+            gui.Menu();
+        }
+    }
+}
